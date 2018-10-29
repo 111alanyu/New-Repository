3fc4e87e96ef8877f06b672a2eda3cdc840fd837
111alanyu <44562288+111alanyu@users.noreply.github.com>
2018-10-28 22:04:45 -0700

    Update Omega.c

--- Omega.c
+++ Omega.c
@@ -1,9 +1,14 @@
+// ConsoleApplication33.cpp : Defines the entry point for the console application.
+
 #include <stdio.h>
 #include <limits.h> 
+#include "stdafx.h"
 
-int main (void) {
+int main(void) {
 
-printf("Hello World");
+	printf("Hello World");
 
+	while (0 < 1) {
 
+	}
 }
